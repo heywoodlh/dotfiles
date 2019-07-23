fdf59dda2156cc3d8c4ae5ffb621bb15c40aed7c
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-07-23 16:19:52 -0600

    updated config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,4 +10,16 @@ end
 
 umask 022
 
+if uname -a | grep -q 'Darwin'
+  set -gx PATH /Users/heywoodlh/go/bin $PATH
+  set -Ux LSCOLORS cxfxcxdxbxcgdggcaghcah
+end
+
+if uname -a | grep -iq 'Linux'
+  set -gx PATH /Users/heywoodlh/go/bin $PATH
+  set -Ux LSCOLORS di=32:ln=35:so=32:pi=33:ex=31:bd=32;46:cd=33;46:su=36;42:sg=30;46:tw=37;42:ow=30;47
+end
+
+
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 source ~/.config/fish/functions/security-functions.fish
