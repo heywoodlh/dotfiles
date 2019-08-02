a15a5e2773401aa4bce6470b62ca48bcc4308a1e
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-08-02 13:32:58 -0600

    switched from Vagrant to Docker

--- config/fish/functions/arch-linux-poweroff.fish
+++
@@ -1,7 +0,0 @@
-function arch-linux-poweroff
-    set working_dir (pwd)
-    set arch_dir ~/.vagrant/arch
-    cd $arch_dir
-    vagrant suspend
-    cd $working_dir
-end
--- config/fish/functions/arch-linux.fish
+++ config/fish/functions/arch-linux.fish
@@ -1,14 +1,3 @@
 function arch-linux
-    set working_dir (pwd)
-    set arch_dir ~/.vagrant/arch
-    mkdir -p $arch_dir
-    cd $arch_dir
-    if test ! -f $arch_dir/Vagrantfile
-        vagrant init archlinux/archlinux
-        rm $arch_dir/Vagrantfile
-        curl 'https://gist.githubusercontent.com/heywoodlh/e24dc94eee7d461c215faa944af13bd7/raw/b0842ecab6340484a84c5f3945dae01fd7acdf8f/arch-vagrant' -o $arch_dir/Vagrantfile
-    end
-    vagrant up
-    vagrant ssh
-    cd $working_dir
+  docker run -it --rm --net host --privileged archlinux/base $argv
 end
