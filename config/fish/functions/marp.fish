a31131bf9893e5318229a2cc149dd8f0d65790dc
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-10-19 21:22:50 -0600

    fixed marp

--- config/fish/functions/marp.fish
+++ config/fish/functions/marp.fish
@@ -1,3 +1,3 @@
 function marp
-  docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli $argv
+  docker run --rm --user (id -u):(id -g) -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli $argv
 end
