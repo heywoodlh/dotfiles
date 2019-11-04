fbb5c563c3eaa11d56c9cbf73ca4052b9fa883ba
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-03 17:16:34 -0700

    added tor browser

--- config/fish/functions/tor-browser-docker.fish
+++ config/fish/functions/tor-browser-docker.fish
@@ -1,9 +1,15 @@
 function tor-browser-docker
-    docker run -d \
+  if uname -a | grep -iq linux
+    if docker ps -a | grep -q 'tor-browser'
+      docker start tor-browser
+    else
+      docker run -d \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -e DISPLAY=unix$DISPLAY \
         --device /dev/snd \
         -v $HOME/Downloads:/root/Downloads \
         --name tor-browser \
         jess/tor-browser
+    end
+  end
 end
