2c0367ba952de1aaf3eaa144f01e6b0982e0b4d5
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-03 20:41:51 -0700

    added firefox

---
+++ config/fish/functions/firefox-docker.fish
@@ -0,0 +1,26 @@
+function firefox-docker
+  if uname -a | grep -iq linux
+    if docker ps -a | grep -q 'firefox'
+      docker start firefox
+    else
+      docker run -d \
+	--memory 2gb \
+	--net host \
+	--cpuset-cpus 0 \
+	-v /etc/localtime:/etc/localtime:ro \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v "$HOME/.firefox/cache:/root/.cache/mozilla" \
+	-v "$HOME/.firefox/mozilla:/root/.mozilla" \
+	-v "$HOME/Downloads:/root/Downloads" \
+	-v "$HOME/Pictures:/root/Pictures" \
+	-v "$HOME/Iso:/root/Iso" \
+	-e "DISPLAY=unix$DISPLAY" \
+	-e GDK_SCALE \
+	-e GDK_DPI_SCALE \
+	--device /dev/snd \
+	--device /dev/dri \
+	--name firefox \
+	jess/firefox "$argv"
+    end
+  end
+end
