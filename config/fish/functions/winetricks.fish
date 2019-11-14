f9e201c151b912bfeeed297c3126a49033b2fb42
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-14 13:22:29 -0700

    fixed wine

---
+++ config/fish/functions/winetricks.fish
@@ -0,0 +1,17 @@
+function winetricks
+  mkdir -p ~/.wine
+  docker run -it --rm \
+         -v ~/.wine:/home/wine/.wine \
+         --net host \
+         -e "DISPLAY=unix$DISPLAY" \
+         -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+         -e GDK_SCALE \
+         -e GDK_DPI_SCALE \
+         --shm-size=4G \
+         --device /dev/snd \
+         --device /dev/dri \
+         --shm-size=4G \
+         --group-add audio \
+         -v (pwd):/home/wine \
+         heywoodlh/wine winetricks $argv
+end
