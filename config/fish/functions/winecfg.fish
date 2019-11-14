aca7665a209e64b8f8b7fb841ff6d26cf999b46d
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-14 12:23:15 -0700

    added wine

---
+++ config/fish/functions/winecfg.fish
@@ -0,0 +1,17 @@
+function wine
+  docker run -it --rm \
+         -v ~/.wine:/home/wine/.wine \
+         --net host \
+         -e "DISPLAY=unix$DISPLAY" \
+         -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+         -e GDK_SCALE \
+         -e GDK_DPI_SCALE \
+         --shm-size=4G \
+         --device /dev/snd \
+         --device /dev/dri \
+         --shm-size=4G \
+         --group-add audio \
+         -v (pwd):/tmp/wine \
+         -w /tmp/wine \
+         heywoodlh/wine winecfg $argv
+end
