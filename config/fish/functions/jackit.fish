f911d7099499141147238d5ed53fdf4763a2f634
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-18 13:33:58 -0700

    added jackit

--- config/fish/functions/jackit.fish
+++ config/fish/functions/jackit.fish
@@ -1,5 +1,5 @@
 if uname -a | grep -iq linux
   function jackit
-    docker run --rm -it --name jackit --privileged -v /dev/bus/usb:/dev/bus/usb heywoodlh/jackit jackit $argv
+    docker run --rm -it --name jackit --privileged -v (pwd):/tmp/jackit -w /tmp/jackit -v /dev/bus/usb:/dev/bus/usb heywoodlh/jackit jackit $argv
   end
 end
