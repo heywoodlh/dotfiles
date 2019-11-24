19f20594612cad03210be9257b839b403f552c0a
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-23 18:08:36 -0700

    added ghex

--- config/fish/functions/ghex.fish
+++ config/fish/functions/ghex.fish
@@ -1,15 +1,11 @@
 function ghex
   if uname -a | grep -iq linux
-    if docker ps -a | grep -q 'ghex'
-      docker start ghex
-    else
-      docker run -d --rm \
-        -v /etc/localtime:/etc/localtime:ro \
-        -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -e DISPLAY=unix$DISPLAY \
-        -v $HOME:/home/ghex \
-        --name ghex \
-        heywoodlh/ghex
-    end
+    docker run -d --rm \
+      -v /etc/localtime:/etc/localtime:ro \
+      -v /tmp/.X11-unix:/tmp/.X11-unix \
+      -e DISPLAY=unix$DISPLAY \
+      -v $HOME:/home/ghex \
+      --name ghex \
+      heywoodlh/ghex
   end
 end
