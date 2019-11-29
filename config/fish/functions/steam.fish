c9bf802b4826c4705baaaef7faf77a155ed63ab2
Spencer Heywood <l.spencer.heywood@protonmail.com>
2019-11-28 17:53:33 -0700

    added steam

--- config/fish/functions/steam.fish
+++ config/fish/functions/steam.fish
@@ -1,23 +1,23 @@
-function iceweasel
+function steam
   if uname -a | grep -iq linux
     if docker ps -a | grep -q 'steam'
       docker start steam
     else
       docker pull heywoodlh/steam:latest
       docker run -d \
-         --net host \
-         --name steam \
-         -e DISPLAY \
-         -e GDK_SCALE \
-         -e GDK_DPI_SCALE \
-         -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-         -v $HOME/.steam:/home/steam/.steam \
-         --device /dev/snd \
-         --device /dev/dri \
-         --shm-size=4G \
-         --group-add audio \
-         -u steam \
-         heywoodlh/steam
+        -v /etc/localtime:/etc/localtime:ro \
+	-v /etc/machine-id:/etc/machine-id:ro \
+	-v /var/run/dbus:/var/run/dbus \
+        --name steam \
+        -e DISPLAY \
+        -e GDK_SCALE \
+        -e GDK_DPI_SCALE \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+        -v $HOME/.steam:/home/steam/.local/share/Steam \
+        --device /dev/snd \
+        --group-add audio \
+        -u steam \
+        heywoodlh/steam
     end
   end
 end
