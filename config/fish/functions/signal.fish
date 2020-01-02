13452cbf308832a813b2975f5850e8afab3c0879
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-01-02 15:03:15 -0700

    added signal

---
+++ config/fish/functions/signal.fish
@@ -0,0 +1,25 @@
+function signal
+  if docker ps -a | grep -q signal-desktop
+    docker start signal-desktop
+  else
+    if ! docker volume ls | grep -q signal_config
+      docker volume create signal_config
+    end
+    docker pull heywoodlh/signal:latest
+    docker run -d \
+      --net host \
+      --name signal-desktop \
+      -e DISPLAY \
+      -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+      -v signal_config:/home/signal/.config/Signal/ \
+      -v $HOME/Downloads:/home/signal/Downloads \
+      -v $HOME/Documents:/home/signal/Documents \
+      -v $HOME/Desktop:/home/signal/Desktop \
+      --device /dev/snd \
+      --device /dev/dri \
+      --shm-size=2G \
+      --group-add audio \
+      -u signal \
+      heywoodlh/signal
+  end
+end
