9885eaad70c7167d93249a8d168c41933fb41fa0
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-01-09 15:44:36 -0700

    updated

--- config/fish/functions/vultr-cli.fish
+++ config/fish/functions/vultr-cli.fish
@@ -1,9 +1,9 @@
 function vultr-cli
   if set -q VULTR_API_KEY
-    docker run --rm -it -e VULTR_API_KEY="$VULTR_API_KEY" heywoodlh/vultr-cli $argv
+    docker run --rm -it -e VULTR_API_KEY="$VULTR_API_KEY" heywoodlh/vultr-cli vultr-cli $argv
   else
     echo "Please enter Vultr API Key:"
     read VULTR_API_KEY
-    docker run --rm -it -e VULTR_API_KEY="$VULTR_API_KEY" heywoodlh/vultr-cli $argv
+    docker run --rm -it -e VULTR_API_KEY="$VULTR_API_KEY" heywoodlh/vultr-cli vultr-cli $argv
   end
 end
