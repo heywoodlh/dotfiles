ef08e107a15b6e4ea6c1d026b4ac10178118c7bc
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-03-25 14:26:18 -0600

    updated volatility

--- config/fish/functions/security-functions.fish
+++ config/fish/functions/security-functions.fish
@@ -366,8 +366,9 @@ function vol.py
   if test -f ~/.volatilityrc
     docker run -it --rm -v ~/.volatilityrc:/root/.volatilityrc -w /data -v (pwd):/data -v /tmp:/tmp heywoodlh/volatility --conf-file /root/.volatilityrc $argv
   else
-    echo 'remember to set needed volatility conf in ~/.volatilityrc!'
-    echo 'paths are relative to (pwd):/data'
+    echo 'Remember to set needed volatility conf in ~/.volatilityrc. Creating new config file now. Please edit it.'
+    echo 'Paths are relative to (pwd):/data'
+    printf "[DEFAULT]\nPROFILE=Win7SP0x86\nLOCATION=file:///data/memory.lime\nPLUGINS=/data\n" > ~/.volatilityrc
     docker run -it --rm -w /data -v (pwd):/data -v /tmp:/tmp heywoodlh/volatility $argv
   end
 end
