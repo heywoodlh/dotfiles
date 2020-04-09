129567b7010fb2e31e6a39e9e79faed7878108aa
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-04-09 16:34:29 -0600

    added aerc

--- config/fish/functions/aerc.fish
+++ config/fish/functions/aerc.fish
@@ -1,4 +1,5 @@
 function aerc
   mkdir -p $HOME/.config/aerc
-  docker run -it --rm --name aerc -v $HOME/.config/aerc:/home/aerc/.config/aerc -e TZ=America/Denver --network host heywoodlh/aerc:latest $argv
+  set EDITOR /usr/bin/vim
+  docker run -it --rm --name aerc -e EDITOR=$EDITOR -v $HOME/.config/aerc:/home/aerc/.config/aerc -e TZ=America/Denver --network host heywoodlh/aerc:latest $argv
 end
