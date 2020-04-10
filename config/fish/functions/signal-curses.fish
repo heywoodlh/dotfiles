823d98a6790302af2fe052d11ea36d2a43132bcf
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-04-10 09:14:34 -0600

    updated

--- config/fish/functions/signal-curses.fish
+++ config/fish/functions/signal-curses.fish
@@ -5,6 +5,6 @@ if uname -a | grep -iq linux
     end
     
     set DBUS_ADDR=(echo "$DBUS_SESSION_BUS_ADDRESS" | cut -d "=" -f2)
-    docker run -it --name signal-curses --env DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" -v signal_curses:/home/signal-curses/.local/share/signal-cli -v /etc/machine-id:/etc/machine-id -v /etc/hostname:/etc/hostname -v "$DBUS_ADDR":"$DBUS_ADDR" --security-opt apparmor=unconfined --rm heywoodlh/signal-curses -u +14358510830
+    docker run -it --name signal-curses --env DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" -v signal_curses:/home/signal-curses/.local/share/signal-cli -v /etc/machine-id:/etc/machine-id -v /etc/hostname:/etc/hostname -v "$DBUS_ADDR":"$DBUS_ADDR" --security-opt apparmor=unconfined --rm heywoodlh/signal-curses $argv
   end
 end
