fe1b161a89f5dd59a2a443d4ef0b7f81b16fe6f9
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-04-13 12:41:48 -0600

    updated for wtf

--- config/fish/functions/wtf.fish
+++ config/fish/functions/wtf.fish
@@ -3,5 +3,5 @@ function wtf
     set WTF_OWM_API_KEY (pass openweathermap_api)
   end
   mkdir -p ~/.config/wtf/
-  docker run --name=wtf --pid host -v ~/Nextcloud/Tasks:/home/wtf/.config/wtf/tasks -v ~/.config/wtf:/home/wtf/.config/wtf -it --rm heywoodlh/wtf -e WTF_OWM_API_KEY=$WTF_OWM_API_KEY --config=/home/wtf/.config/wtf/config.yml 
+  docker run --name=wtf --pid host -v ~/Nextcloud/Tasks:/home/wtf/.config/wtf/tasks -v ~/.config/wtf:/home/wtf/.config/wtf -e WTF_OWM_API_KEY=$WTF_OWM_API_KEY -it --rm heywoodlh/wtf --config=/home/wtf/.config/wtf/config.yml 
 end
