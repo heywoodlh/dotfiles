efa252d35621ec68c3016a2ea1fd96c47cbaa492
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-04-23 11:54:13 -0600

    updated

--- config/fish/functions/aerc.fish
+++ config/fish/functions/aerc.fish
@@ -1,8 +1,20 @@
 function aerc
   mkdir -p ~/.config/aerc/scripts
+  mkdir -p ~/.config/aerc/filters
   mkdir -p ~/.config/khard/contacts
   mkdir -p ~/.gnupg
   mkdir -p ~/.password-store
   set EDITOR /usr/bin/vim
-  docker run -it --rm --name aerc -e EDITOR=$EDITOR -v ~/.config/aerc/scripts:/home/aerc/.config/aerc/scripts -v ~/.config/khard:/home/aerc/.config/khard -v ~/.password-store:/home/aerc/.password-store -v ~/.gnupg:/home/aerc/.gnupg -v $HOME/.config/aerc:/home/aerc/.config/aerc -e TZ=America/Denver --network host heywoodlh/aerc:latest $argv
+  
+  docker run -it --rm --name aerc \
+    -e EDITOR=$EDITOR \
+    -v ~/.config/aerc/filters:/usr/share/aerc/filters \
+    -v ~/.config/aerc/scripts:/home/aerc/.config/aerc/scripts \
+    -v ~/.config/khard:/home/aerc/.config/khard \
+    -v ~/.password-store:/home/aerc/.password-store \
+    -v ~/.gnupg:/home/aerc/.gnupg \
+    -v $HOME/.config/aerc:/home/aerc/.config/aerc \
+    -e TZ=America/Denver \
+    --network host \
+    heywoodlh/aerc:latest $argv
 end
