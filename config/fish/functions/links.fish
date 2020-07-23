01e30318766daffce2086e46bd6d8f56a4386659
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-07-22 19:10:20 -0600

    updated links function

--- config/fish/functions/links.fish
+++ config/fish/functions/links.fish
@@ -1,8 +1,3 @@
 function links
-  set architecture (uname -m)
-  if [ $architecture = "x86_64" ]
-    docker run heywoodlh/links $argv
-  else
-    docker run heywoodlh/links:arm $argv
-  end
+  docker run -it --rm heywoodlh/links:arm $argv
 end
