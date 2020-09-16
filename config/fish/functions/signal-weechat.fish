36d016d01996c9f460a8f43bae9001689a6b924b
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-09-16 16:53:36 -0600

    updated

--- config/fish/functions/signal-weechat.fish
+++ config/fish/functions/signal-weechat.fish
@@ -1,9 +1,9 @@
 function signal-weechat 
   signald
-  mkdir -p ~/.weechat
+  docker volume signal-weechat
   docker run -it --rm \
     -v ~/.docker/signald/run:/var/run/signald \
     -v ~/.config/signald:/weechat/.config/signald \
-    -v ~/.weechat:/weechat/.weechat \
+    -v signal-weechat:/weechat/.weechat \
     heywoodlh/signal-weechat
 end
