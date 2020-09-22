bfd1b46e2123aba1ff57e11da8d9d37ce0b58cfa
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-09-22 11:52:58 -0600

    updated

--- config/fish/functions/libreoffice.fish
+++ config/fish/functions/libreoffice.fish
@@ -2,24 +2,31 @@ function libreoffice
   if ! docker volume ls | grep -iq libreoffice
     docker volume create libreoffice
   end
+
+  if ! docker network ls | grep -iq libreoffice
+    docker network create libreoffice
+  end
   
+
   docker run -d --name=libreoffice --restart=unless-stopped \
     -p 9980:9980 \
+    --network libreoffice \
     -v libreoffice:/etc/loolwsd \
     -e DONT_GEN_SSL_CERT=1 \
     -e extra_params="--o:ssl.enable=false" \
+    -e domain="nextcloud" \
+    --cap-add MKNOD \
+    --cap-add SYS_ADMIN \
     libreoffice/online:latest
 
   if ! docker volume ls | grep -iq nextcloud
     docker volume create nextcloud
   end
   docker run --name=nextcloud -d --restart=unless-stopped \
-    --link libreoffice:libreoffice \
+    --network libreoffice \
     -v nextcloud:/var/www/html \
     -e NEXTCLOUD_TRUSTED_DOMAINS="nextcloud" \
-    -e NEXTCLOUD_ADMIN_USER="admin" \
-    -e NEXTCLOUD_ADMIN_PASSWORD="admin" \
-    -p 9080:80 \
+    -p 80:80 \
     nextcloud:stable-apache
 
 end
