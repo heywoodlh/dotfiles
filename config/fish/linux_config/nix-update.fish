1354284ccb918007d4f8f54587e3a49287c8b0dd
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-10-12 14:21:04 -0600

    updated nix-update function

--- config/fish/linux_config/nix-update.fish
+++ config/fish/linux_config/nix-update.fish
@@ -3,6 +3,7 @@ function nix-update
   cd /etc/nixos
   git pull origin master
   sudo nixos-rebuild switch
+  # remove any generation older than 30 days
   sudo nix-collect-garbage --delete-older-than 30d
   cd $workingDir
 end
