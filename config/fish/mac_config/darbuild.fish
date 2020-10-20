88bcf4e00a991bbc86637aae6076ad71cab22442
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-10-20 09:23:39 -0600

    updated darbuild

--- config/fish/mac_config/darbuild.fish
+++ config/fish/mac_config/darbuild.fish
@@ -1,4 +1,5 @@
 function darbuild
   sudo chown -R heywoodlh:admin /nix
+  nix-channel --update
   darwin-rebuild switch -I "darwin-config=$HOME/opt/nixos-builds/darwin/darwin-configuration.nix"
 end
