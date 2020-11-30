072a61f8f8322036a95005c9e5ad9da1f8de456d
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-11-30 14:53:57 -0700

    updated m4a2mp3 fish function

--- config/fish/functions/m4a2mp3.fish
+++ config/fish/functions/m4a2mp3.fish
@@ -1,13 +1,17 @@
 function m4a2mp3
-  if [ -z $argv[1] ]
+  set input "$argv[1]"
+  set output "$argv[2]"
+
+  set error "false"
+
+  if test -z $input; or test -z $output
     set error "true"
   end
-  if [ -z $argv[2] ]
-    set error "true"
-  end
-  if [ $error != "true" ]
-    ffmpeg -i $argv[1] -c:a libmp3lame -q:a 8 $argv[2]
+
+  if test $error != "true"
+    ffmpeg -i "$input" -c:a libmp3lame -q:a 8 "$output"
   else
     echo "usage: m4a2mp3 input.m4a output.mp3"
   end
+
 end
