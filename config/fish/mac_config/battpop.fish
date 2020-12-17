48ff2aaa1fdb40a9a6231e35fe7e4e70f1efc39a
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-12-17 15:16:43 -0700

    updated battpop

--- config/fish/mac_config/battpop.fish
+++ config/fish/mac_config/battpop.fish
@@ -1,4 +1,4 @@
 function battpop
   set battery_level (pmset -g batt | grep 'InternalBattery' | awk '{print $3 $4}' | sed 's/;/ /g')
-  osascript -e "tell app \"System Events\" to display dialog \"BATTERY LEVEL: $battery_level\""
+  osascript -e "display notification \"$battery_level\" with title \"Battery Level\""
 end
