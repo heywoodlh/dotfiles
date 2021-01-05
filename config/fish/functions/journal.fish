4ff421e5061c4df2842326f4b1c2ddb5cd7ae5bd
Spencer Heywood <l.spencer.heywood@protonmail.com>
2021-01-05 14:05:56 -0700

    journal

--- config/fish/functions/journal.fish
+++ config/fish/functions/journal.fish
@@ -1,17 +1,3 @@
 function journal
-  set target "$HOME/Sync/journal"
-  
-  set year (date +%Y)
-  set month (date +%h)
-  set day (date +%d)
-  set time (date +%H:%M:%S)
-
-  mkdir -p "$target/$year/$month"
-  set file "$target/$year/$month/$day.txt"
-  
-  printf "[$time]\n" >> $file
-
-  vim + $file
-
-  printf "\n" >> $file
+  mosh heywoodlh@min /home/heywoodlh/.local/bin/journal
 end
