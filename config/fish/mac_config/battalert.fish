fb8e1e8557d10202941d4352573fc166a216c785
Spencer Heywood <l.spencer.heywood@protonmail.com>
2021-01-09 18:06:29 -0700

    added battalert

---
+++ config/fish/mac_config/battalert.fish
@@ -0,0 +1,9 @@
+function battalert
+  set threshold 10
+  
+  set battery_level (pmset -g batt | grep 'InternalBattery' | awk '{print $3}' | sed 's/;/ /g' | tr -d '%')
+  
+  if [ $battery_level -lt $threshold ]
+    osascript -e "display notification \"Battery level less than $threshold%\" with title \"Battery Level\""
+  end
+end
