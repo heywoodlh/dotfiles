9d624b69c6d475581012751610e4c4802508f4a8
Spencer Heywood <l.spencer.heywood@protonmail.com>
2021-01-19 15:12:56 -0700

    updated gist function

--- config/fish/functions/gist.fish
+++ config/fish/functions/gist.fish
@@ -2,8 +2,15 @@ function gist
   set usage "usage: gist file.txt --private"
   set arguments (count $argv)
   
+  set private "false"
+
   if [ $arguments -gt 0 ]
     set file_name $argv[1]
+    set file_exists "true"
+    if [ ! -e $file_name ]
+      set file_exists "false"
+      set file_name "/tmp/$file_name"
+    end
     if [ $arguments -gt 1 ]
       if echo $argv | grep -q '\-\-private'
         set private "true"
@@ -19,13 +26,14 @@ function gist
  
   
   if [ -z "$run_error" ]
-    vim /tmp/$file_name 
-    if [ $private != "true" ]
-      gh gist create --public /tmp/$file_name -d $file_name
-    else
-      gh gist create /tmp/$file_name -d $file_name
+    if [ $file_exists = "false" ]
+      vim $file_name 
     end
-    rm /tmp/$file_name
+    if [ $private != "true" ]
+      gh gist create --public $file_name -d $file_name
+    else
+      gh gist create $file_name -d $file_name
+    end
   else
     echo $usage
   end
